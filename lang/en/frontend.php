9212b44bff8b56fe58ac795e19bab4907084eb18
Shamima Haque <haqueshamima34@gmail.com>
2024-02-05 22:26:59 +0600

    fourth commit

--- lang/en/frontend.php
+++ lang/en/frontend.php
@@ -24,7 +24,7 @@ return [
     'contact' =>'CONTACT',
     'contact_title'=>'PT FAITO RACING DEVELOPMENT INDONESIA',
     'head_office'=>'Head Office :',
-    'address'=>'The Boulevard No. 53 Jakarta Garden City Jakarta Timur 13910',
+    'address_head_office'=>'The Boulevard No. 53 Jakarta Garden City Jakarta Timur 13910',
     'available_for'=>'AVAILABLE FOR',
     'say_hello'=>'SAY HELLO!',
     'join_reseller' =>'JOIN RESELLER',
@@ -36,23 +36,47 @@ return [
     'motor_type' =>'Motor Type',
     'message'=>'Message',
     'become_reseller'=>'Become Reseller',
-    ''=>'Become Reseller Description',
-    ''=>'Workshop Information',
-    ''=>'Address',
-    ''=>'Photo Outside Workshop No file chosenUpload Here',
-    ''=>'Photo Inside Workshop No file chosenUpload Here',
-    ''=>'Photo Selfie Workshop No file chosenUpload Here',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
-    ''=>'',
+    'become_reseller_des'=>'Become Reseller Description',
+    'workshop_information'=>'Workshop Information',
+    'address'=>'Address',
+    'photo_outside_workshop'=>'Photo Outside Workshop',
+    'no_file_chosen'=>'No file chosen',
+    'upload_here'=>'Upload Here',
+    'photo_inside_workshop'=>'Photo Inside Workshop',
+    'photo_selfie_workshop'=>'Photo Selfie Workshop',
+    'send_message'=>'Send Message',
 
- ]
+ ],
+
+ 'story'=>[
+    'faitology'=>[
+        'title'=>'WHO WE ARE',
+        'sub_title'=>'WHERE RIDING EXPERIENCE COUNT',
+        'faitology_des_1'=>'Faito presents quality and innovative motorcycle spare parts products, each product is made with detailed accuracy standards, starting from the composition of materials and dimensional accuracy so that quality products are present.',
+        'faitology_des_2'=>'Faito focus creates innovative products by considering performance, durability and comfort to provide a better riding experience.',
+        'vission'=>'VISION',
+        'vission_des'=>'Making Faito a leader in the worldwide spare parts market',
+        'mission'=>'MISSION',
+        'mission_des_1'=>'Providing high-performance parts combined with high-quality materials at very competitive prices.',
+        'mission_des_2'=>'We have established a strong and reliable network throughout the world, spreading our influence in this highly competitive industry.',
+        ''=>'MILESTONES',
+        ''=>'Faito was established, the company is managed by a board of directors who have been involved in the motorcycle industry for more than 30 years.',
+        ''=>'Faito supports the local Petronas AAM Malaysian Cub Prix Championship, and participate in the race as the main sponsor for Faito YSP racing team and AAP Maz Oil HRC Faito racing team',
+        ''=>'Faito opening branch in Indonesia',
+        ''=>'Faito partner with Air-Asia-SIC -Ajo Team in MotoGP - Moto3',
+        ''=>'FAITO co-sponsored Racing GIATMARA Garage Northern Team',
+        ''=>'Faito partner with Tsukigi Racing in Asia Road Racing Championship',
+        ''=>'Roadshow to Japan',
+        ''=>'Faito opening of its first showroom in Caloocan, Philippines',
+        ],
+
+    'faitotech'=>[
+       ''=>'',
+       ''=>'',]
+        
+    ],
+
+
+
+
 ];
